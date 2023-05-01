cadaaae9be90522b423c9078356a016fe880e3e1
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-05-01 15:29:34 +0900

    refactor

--- front/src/components/templates/Layout.tsx
+++ front/src/components/templates/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
+import styles from './Layout.module.css'
 import { Footer } from '@/components/organisms/Footer'
 import { Header } from '@/components/organisms/Header'
-import styles from './Layout.module.css'
 
 type Props = {
   children?: ReactNode
