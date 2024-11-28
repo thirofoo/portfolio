828c7b6d0854af7bbf8a4d5fd7975d0fa694b0c9
thirofoo <83126064+thirofoo@users.noreply.github.com>
2024-11-28 17:40:23 +0900

    Revert "feat: create slide button (#55)"
    This reverts commit fb18dd0cf053e8ad89f0a7ca092d925b42f203d7.

--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -3,8 +3,8 @@ module.exports = {
   bg1: 'white',
   bg2: '#f0f0f0',
   bg3: '#fafafa',
-  bg4: '#f2f2f2',
-  
+  bg4: 'black',
+
   // bg-color (dark)
   // dbg1: '#182436',
   // dbg2: '#283446',
@@ -12,13 +12,13 @@ module.exports = {
   dbg1: '#151515',
   dbg2: '#252525',
   dbg3: '#090909',
-  dbg4: '#555555',
 
   // text-color
   txt1: 'black',
   txt2: '#cad1df',
 
   // shadow-color
+  // sh1: '#9da5b2', // 薄
   sh1: '#7d8592', // 薄
   sh2: 'black', // 濃
   sh3: '#859fa2', // 青目
